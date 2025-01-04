ac5cb491be20a8c70c5f44bb7593337730f40212
João_A.11 <jarlpereira29@gmail.com>
2025-01-04 14:18:59 -0300

    Troca lingaguem da MainActivity para java

---
+++ app/src/main/java/com/joao_ar11/frasesdodia/MainActivity.java
@@ -0,0 +1,14 @@
+package com.joao_ar11.frasesdodia;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+public class MainActivity extends ComponentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
